6c5124fe1f2bdf30657860e52fa1ac106fc02583
Jayden Callan <jqcallan04@gmail.com>
2022-07-09 20:08:23 -0600

    completed exercise

--- Variables Exercise/Program.cs
+++ Variables Exercise/Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace VariablesExercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = "John";
+            int age = 20;
+            char lastInitial = 'Q';
+            bool married = true;
+            double gpa = 3.5;
+            decimal height = 5.5m;
+
+            Console.WriteLine($"{name} {lastInitial} is a {age} year old. It is {married} that he is married. His GPA was {gpa} and his height is {height} feet.");
+        }
+    }
+}
